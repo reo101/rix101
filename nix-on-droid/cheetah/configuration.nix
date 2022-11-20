9a6d5999cda434345d595250a459be1d41e7ac27
reo101 <pavel.atanasov2001@gmail.com>
2022-11-20 17:23:58 +0200

    feat(config): initial
    Based on `https://github.com/Misterio77/nix-starter-configs/tree/cce47e4df5225d83a61b56c209f8cdb2fbc26ace/standard`
    Adds (experimental) support for `nix-on-droid` and `nix-on-darwin`
    Includes support for multiple hosts of each base
    Adds config for `cheetah` on `nix-on-droid`

---
+++ nix-on-droid/cheetah/configuration.nix
@@ -0,0 +1,31 @@
+{ inputs, outputs, lib, config, pkgs, ... }:
+
+{
+  environment.packages = with pkgs; [];
+
+  # Backup etc files instead of failing to activate generation if a file already exists in /etc
+  environment.etcBackupExtension = ".bak";
+
+  # Remove motd
+  environment.motd = null;
+
+  # # Set $EDITOR
+  # environment.variables.EDITOR = "nvim";
+
+  # Read the changelog before changing this value
+  system.stateVersion = "22.05";
+
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+
+  time.timeZone = "Europe/Sofia";
+
+  home-manager = {
+    config = ./home.nix;
+    backupFileExtension = "hm-bak";
+    useGlobalPkgs = true;
+
+    # extraOptions = { inherit inputs; };
+  };
+}
---
+++ nix-on-droid/cheetah/home.nix
@@ -0,0 +1,113 @@
+{ inputs ? {}, outputs ? {}, lib, config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home = {
+    username = "nix-on-droid";
+    # username = "reo101";
+    homeDirectory = "/data/data/com.termux.nix/files/home";
+    stateVersion = "22.05";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  home.packages = with pkgs; [
+    # neovim
+    # clang
+    gcc
+
+    diffutils
+    findutils
+    utillinux
+    tzdata
+    hostname
+    man
+    ncurses
+    gnugrep
+    gnupg
+    gnused
+    gnutar
+    bzip2
+    gzip
+    xz
+    zip
+    unzip
+
+    # Bling
+    onefetch
+    neofetch
+
+    # Utils
+    ripgrep
+
+    # Passwords
+    pass
+    passExtensions.pass-otp
+
+    # Dhall
+    dhall
+    dhall-lsp-server
+  ];
+
+  # nixpkgs = {
+  #   overlays = [
+  #     inputs.neovim-nightly-overlay.overlay
+  #     inputs.zig-overlay.overlay
+  #   ];
+  #
+  #   config.allowUnfree = true;
+  # };
+
+  # programs.zig = {
+  #   enable = true;
+  #   package = pkgs.zig;
+  # };
+
+  programs.neovim = {
+    enable = true;
+    # package = neovim-nightly-overlay.neovim-nightly;
+    # package = pkgs.neovim-nightly;
+    # defaultEditor = true;
+
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+
+    withPython3 = false;
+    withNodeJs = false;
+    withRuby = false;
+
+    # neovimRcContent = "";
+
+    extraPackages = with pkgs; [
+        tree-sitter
+        rnix-lsp
+        # sumneko-lua-language-server
+        # stylua
+        # texlab
+        # rust-analyzer
+    ];
+  };
+
+  home.file = {
+    ".config/nvim" = {
+      recursive = true;
+      source = /data/data/com.termux.nix/files/home/.local/src/reovim;
+    };
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "reo101";
+    # userName = "Pavel Atanasov";
+    userEmail = "pavel.atanasov2001@gmail.com";
+  };
+
+  services.gpg-agent = {
+    enable = true;
+    defaultCacheTtl = 1800;
+    enableSshSupport = true;
+  };
+}
