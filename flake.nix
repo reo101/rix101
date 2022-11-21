040215f423aa245f27be22ffa9bf68a6117fcc57
reo101 <pavel.atanasov2001@gmail.com>
2022-11-21 09:53:32 +0200

    feat(main): `forAllSystems`,`packages`,`devShells`
    Reenable

--- flake.nix
+++ flake.nix
@@ -52,25 +52,25 @@
             , ...
             } @ inputs:
     let
-      # forAllSystems = nixpkgs.lib.genAttrs [
-      #   "aarch64-linux"
-      #   "i686-linux"
-      #   "x86_64-linux"
-      #   "aarch64-darwin"
-      #   "x86_64-darwin"
-      # ];
       inherit (self) outputs;
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
     in
     rec {
-      # packages = forAllSystems (system:
-      #   let pkgs = nixpkgs.legacyPackages.${system};
-      #   in import ./pkgs { inherit pkgs; }
-      # );
+      packages = forAllSystems (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in import ./pkgs { inherit pkgs; }
+      );
 
-      # devShells = forAllSystems (system:
-      #   let pkgs = nixpkgs.legacyPackages.${system};
-      #   in import ./shell.nix { inherit pkgs; }
-      # );
+      devShells = forAllSystems (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in import ./shell.nix { inherit pkgs; }
+      );
 
       overlays = import ./overlays;
       nixosModules = import ./modules/nixos;
