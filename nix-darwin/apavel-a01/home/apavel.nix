96305f635e7a664edc554f799e8538679a31b6a8
reo101 <pavel.atanasov2001@gmail.com>
2022-11-25 14:51:29 +0200

    feat(darwin): enable home-manager support
    Add `home-manager` config for `apavel`
    Add some `configuration.nix` options for `apavel-a01`:
        - `FiraCode` nerdfont
        - Basic keyboard config
        - TouchID for `sudo`

---
+++ nix-darwin/apavel-a01/home/apavel.nix
@@ -0,0 +1,62 @@
+{ inputs, lib, config, pkgs, ... }:
+
+{
+  home = {
+    # username = "apavel";
+    # homeDirectory = "/Users/apavel";
+    stateVersion = "22.05";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  home.packages = with pkgs; [
+    # Dhall
+    dhall
+    dhall-lsp-server
+
+    # Nix
+    rnix-lsp
+    nil
+
+    # Zig
+    zigpkgs.master
+    inputs.zls-overlay.packages.x86_64-darwin.default
+  ];
+
+  nixpkgs = {
+    overlays = [
+      inputs.zig-overlay.overlays.default
+    ];
+
+    config.allowUnfree = true;
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "apavel";
+    userEmail = "apavel@vmware.com";
+    signing = {
+      signByDefault = true;
+      key = "0x52F3E1D376F692C0";
+    };
+  };
+
+  # services.gpg-agent = {
+  #   enable = true;
+  #   defaultCacheTtl = 1800;
+  #   enableSshSupport = true;
+  # };
+
+  # home.file.".stack/config.yaml".text = lib.generators.toYAML {} {
+  #   templates = {
+  #     scm-init = "git";
+  #     params = with config.programs.git; {
+  #       author-name = userName;
+  #       author-email = userEmail;
+  #       github-username = userName;
+  #     };
+  #   };
+  #   nix.enable = true;
+  # };
+}
