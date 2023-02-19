cbac8b2754bca27158e4b3456e867ccce21bc262
reo101 <pavel.atanasov2001@gmail.com>
2023-02-19 13:11:41 +0200

    fix(devShells): fix output of `default` shell

--- shells/default/default.nix
+++ shells/default/default.nix
@@ -1,15 +1,13 @@
 # Shell for bootstrapping flake-enabled nix and other tooling
 { pkgs
 , ...
-}: {
-  default = pkgs.mkShell {
-    NIX_CONFIG = ''
-      extra-experimental-features = nix-command flakes repl-flake
-    '';
-    nativeBuildInputs = with pkgs; [
-      nix
-      home-manager
-      git
-    ];
-  };
+}: pkgs.mkShell {
+  NIX_CONFIG = ''
+    extra-experimental-features = nix-command flakes repl-flake
+  '';
+  nativeBuildInputs = with pkgs; [
+    nix
+    home-manager
+    git
+  ];
 }
