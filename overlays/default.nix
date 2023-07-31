918ba0ea29becc1de2bd76d7e42bc30df75449f1
reo101 <pavel.atanasov2001@gmail.com>
2023-07-31 07:51:55 +0300

    feat(overlays): add discord & `reo101` maintainer

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,7 +2,9 @@
 { inputs, outputs, ... }:
 {
   # This one brings our custom packages from the 'pkgs' directory
-  additions = final: _prev: import ../pkgs { pkgs = final; };
+  additions = final: _prev: import ../pkgs {
+    pkgs = final;
+  };
 
   # This one contains whatever you want to overlay
   # You can change versions, add patches, set compilation flags, anything really.
@@ -11,20 +13,31 @@
     # example = prev.example.overrideAttrs (oldAttrs: rec {
     # ...
     # });
+
+    lib = prev.lib // {
+      maintainers = {
+        reo101 = {
+          name = "Pavel Atanasov";
+          email = "pavel.atanasov2001@gmail.com";
+          github = "reo101";
+          githubId = "37866329";
+          keys = [
+            {
+              fingerprint = "8A29 0250 C775 7813 1DD1  DC57 7275 0ABE E181 26D0";
+            }
+          ];
+        };
+      };
+    };
+
     river = prev.river.overrideAttrs (oldAttrs: rec {
       xwaylandSupport = true;
     });
 
-    # armcord = prev.armcord.overrideAttrs (oldAttrs: let
-    #   openasar = final.fetchurl {
-    #     url = "https://github.com/GooseMod/OpenAsar/releases/download/nightly/app.asar";
-    #     sha256 = final.lib.fakeSha256;
-    #   };
-    # in rec {
-    #   postInstall = (oldAttrs.postInstall or "") ++ ''
-    #     install -v "${openasar}" "$out/opt/Discord/resources/app.asar"
-    #   '';
-    # });
+    discord = prev.discord.override {
+      withOpenASAR = true;
+      withVencord = true;
+    };
 
     prismlauncher = prev.prismlauncher.overrideAttrs (oldAttrs: {
       patches = (oldAttrs.patches or [ ]) ++ [
