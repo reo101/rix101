8cd75579d272b277074e45c2e702b95c917efd35
reo101 <pavel.atanasov2001@gmail.com>
2023-09-06 21:23:50 +0300

    feat(pkgs): add `pngpaste`

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -11,6 +11,7 @@
   fennel-language-server = pkgs.callPackage ./fennel-language-server { };
   parinfer-rust = pkgs.callPackage ./parinfer-rust { };
   pest-ide-tools = pkgs.callPackage ./pest-ide-tools { };
+  pngpaste = pkgs.callPackage ./pngpaste { };
   srtool-cli = pkgs.callPackage ./srtool-cli { };
   swww = pkgs.callPackage ./swww { };
   vim-fmi-cli = pkgs.callPackage ./vim-fmi-cli { };
---
+++ pkgs/pngpaste/default.nix
@@ -0,0 +1,21 @@
+{ lib, darwin, fetchFromGitHub, ... }:
+
+darwin.apple_sdk.stdenv.mkDerivation rec {
+  name = "pngpaste";
+  version = "0.2.3";
+  src = fetchFromGitHub {
+    owner = "jcsalterego";
+    repo = "${name}";
+    rev = "67c39829fedb97397b691617f10a68af75cf0867";
+    sha256 = "sha256-uvajxSelk1Wfd5is5kmT2fzDShlufBgC0PDCeabEOSE=";
+  };
+  buildInputs = with darwin.apple_sdk.frameworks; [
+    AppKit
+    Cocoa
+    Foundation
+  ];
+  installPhase = ''
+    mkdir -p $out/bin
+    mv pngpaste $out/bin/pngpaste
+  '';
+}
