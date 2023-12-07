0fae14d92a8fcdc82f95216e069969d298fdd3ec
reo101 <pavel.atanasov2001@gmail.com>
2023-12-08 01:18:51 +0200

    chore(transmission)!: enable `rpc` endpoint

--- machines/nixos/x86_64-linux/jeeves/jellyfin.nix
+++ machines/nixos/x86_64-linux/jeeves/jellyfin.nix
@@ -4,14 +4,20 @@
     tremc
   ];
 
+  networking.extraHosts = ''
+    127.0.0.1 jeeves
+  '';
+
   services = {
     transmission = {
       enable = true;
       openRPCPort = true;
+      # TODO: `credentialsFile` for RPC password with agenix
       settings = {
         download-dir = "/data/torrents/download";
         incomplete-dir = "/data/torrents/incomplete";
         incomplete-dir-enabled = true;
+        rpc-bind-address = "0.0.0.0";
         rpc-whitelist = "127.0.0.1,192.168.*.*,10.100.0.*";
       };
     };
