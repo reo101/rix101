be2ad3ccfd943304a632ab5355fea6c1693f6480
reo101 <pavel.atanasov2001@gmail.com>
2024-06-01 14:44:07 +0300

    fix(homebrew)!: do not reinstall on activation
    `/usr/local/bin/brew` was the path for `x86_64-darwin`
    `aarch64-darwin` uses `/opt/homebrew/bin/brew`
    Use `${config.homebrew.brewPrefix}` to support both
    See <https://github.com/LnL7/nix-darwin/blob/c0d5b8c54d6828516c97f6be9f2d00c63a363df4/modules/homebrew.nix#L561>

--- modules/nix-darwin/brew/default.nix
+++ modules/nix-darwin/brew/default.nix
@@ -20,7 +20,7 @@ in
       if ! xcode-select --version 2>/dev/null; then
         $DRY_RUN_CMD xcode-select --install
       fi
-      if ! /usr/local/bin/brew --version 2>/dev/null; then
+      if ! ${config.homebrew.brewPrefix}/brew --version 2>/dev/null; then
         $DRY_RUN_CMD /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
       fi
     '';
