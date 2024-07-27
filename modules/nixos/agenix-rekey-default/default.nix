1b913fcf9b6442b9b152b8d86b65050c5c487eb2
reo101 <pavel.atanasov2001@gmail.com>
2024-07-27 13:10:26 +0300

    chore(docs): update old filepath

--- modules/nixos/agenix-rekey-default/default.nix
+++ modules/nixos/agenix-rekey-default/default.nix
@@ -1,7 +1,7 @@
 { inputs, outputs, lib, pkgs, config, options, ... }:
 {
   config = {
-    # NOTE: `(r)agenix` and `agenix-rekey` modules are imported by `../../../nix/configurations.nix`
+    # NOTE: `(r)agenix` and `agenix-rekey` modules are imported by `../../../modules/flake/configurations.nix`
     age.rekey = {
       masterIdentities = lib.mkDefault [ "${inputs.self}/secrets/privkey.age" ];
       storageMode      = lib.mkDefault "local";
