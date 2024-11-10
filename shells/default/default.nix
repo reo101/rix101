df5a29bab6aa5917a6f17ffc22919dbc9c906c61
reo101 <pavel.atanasov2001@gmail.com>
2024-11-10 19:55:57 +0200

    feat(shells)!: add modified `darwin-rebuild` that uses `nix-enraged`

--- shells/default/default.nix
+++ shells/default/default.nix
@@ -15,5 +15,15 @@
     rage
     inputs.agenix-rekey.packages.${pkgs.hostPlatform.system}.agenix-rekey
     age-plugin-yubikey
+  ] ++ lib.optionals pkgs.stdenv.hostPlatform.isDarwin [
+    # "${inputs.nix-darwin.outPath}/pkgs/nix-tools/default.nix" |> import |> (p: pkgs.callPackage p {}) |> builtins.getAttr "darwin-rebuild"
+    (lib.pipe inputs.nix-darwin.outPath [
+      (f: "${f}/pkgs/nix-tools/default.nix")
+      import
+      (p: pkgs.callPackage p {
+        nixPackage = (nix-enraged.override { monitored = false; });
+      })
+      (builtins.getAttr "darwin-rebuild")
+    ])
   ];
 }
