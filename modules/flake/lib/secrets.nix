cd1c04cdbb8cab2425b7e44f71a8658ceeb96fa3
reo101 <pavel.atanasov2001@gmail.com>
2024-11-15 23:47:56 +0200

    feat(secrets): make `rageImportEncrypted` take a `hostPlatform` argument
    - Used to filter out the `YubiKey`s when on `nix-on-droid`

--- modules/flake/lib/secrets.nix
+++ modules/flake/lib/secrets.nix
@@ -2,12 +2,23 @@
 
 {
   config.lib = {
-    rageImportEncrypted =
+    # TODO: move into `default-generators.nix` or similar
+    rageImportEncrypted = masterIdentities: hostPlatform:
       assert lib.assertMsg (builtins ? extraBuiltins.rageImportEncrypted)
       "The extra builtin `rageImportEncrypted` is not available, so secrets cannot be decrypted. Did you forget to use `nix-enraged`? (or manually set up `nix-plugins` and `extra-builtins-file`)";
       builtins.extraBuiltins.rageImportEncrypted
-       (lib.map
-         ({ identity, pubkey }: identity)
-         config.flake.secretsConfig.masterIdentities);
+        (lib.pipe config.flake.secretsConfig.masterIdentities [
+          (lib.filter
+            ({ identity, pubkey }:
+              # NOTE: cannot not use `YubiKey`s on `nix-on-droid`
+              # TODO: better detection of `nix-on-droid`
+              if hostPlatform.isLinux && hostPlatform.isAarch64
+              # TODO: better detection of `yubikey`
+              then !lib.hasInfix "yubikey" identity
+              else true))
+          (lib.map
+            ({ identity, pubkey }:
+              identity))
+        ]);
   };
 }
