97c7ce1aa281b0fbe440a6392eefe3eb9ca28ba0
reo101 <pavel.atanasov2001@gmail.com>
2025-01-25 03:29:50 +0200

    feat(scm)!: update
    - Add `nvim` option for `jj`
      - Custom `merge-tool`
      - Custom `editor`
    - Use `nvim -b` (custom blocking `flatten.nvim`)
    - Add `gh` when `git` option is enabled
    - Add custom `revset-aliases`
    - Add custom `aliases`
    - Add custom `template-aliases`
    - Add custom `templates`

--- modules/home-manager/reo101-scm/default.nix
+++ modules/home-manager/reo101-scm/default.nix
@@ -23,6 +23,9 @@ in
         };
         jj = {
           enable = mkEnableOption "reo101 jj setup";
+          nvim = mkEnableOption "Integration with `hunk.nvim` and `jj-diffconflicts`" // {
+            default = true;
+          };
         };
       };
     };
@@ -41,9 +44,10 @@ in
       builtins.concatLists [
         (optionals cfg.git.enable [
           # git
+          gh
         ])
         (optionals cfg.jj.enable [
-          jujutsu
+          # jujutsu
           watchman
         ])
       ];
@@ -78,8 +82,9 @@ in
         };
         git = {
           fetch = ["origin" "upstream"];
-          push = "github";
+          push = "origin";
           private-commits = "description(glob:'wip:*')";
+          push-bookmark-prefix = "reo101/";
         };
         signing = {
           backend = "gpg";
@@ -98,24 +103,73 @@ in
         };
         ui = {
           color = "always";
+        } // mkIf cfg.jj.nvim {
           # pager = "nvim";
-          editor = "nvim";
+          editor = "nvim -b";
           diff-editor = [
             "nvim"
             "-c"
             "DiffEditor $left $right $output"
           ];
         };
+        merge-tools = {
+          diffconflicts = mkIf cfg.jj.nvim {
+            program = "nvim";
+            merge-args = [
+              "-c" "let g:jj_diffconflicts_marker_length=$marker_length"
+              "-c" "JJDiffConflicts!" "$output" "$base" "$left" "$right"
+            ];
+            # NOTE: no history view
+            # merge-args = [
+            #   "-c" "JJDiffConflicts!" "$output"
+            # ];
+          };
+        };
+        revset-aliases = {
+          "stragglers" = "(visible_heads() & mine()) ~ trunk()";
+          "bases" = "master | main";
+          "downstream(x,y)" = "(x::y) & y";
+          "branches" = "downstream(trunk(), bookmarks()) & mine()";
+          "branchesandheads" = "branches | (heads(trunk()::) & mine())";
+          "currbranch" = "latest(branches::@- & branches)";
+          "nextbranch" = "roots(@:: & branchesandheads)";
+        };
+        aliases = {
+          "dragmain" = ["bookmark" "set" "main" "-r" "@-"];
+          "sync" = ["git" "fetch" "--all-remotes"];
+          "evolve" = ["rebase" "--skip-emptied" "-d" "trunk()"];
+          "pullup" = ["evolve" "-s" "all:stragglers"];
+
+          "xl" = ["log" "-r" "all()"];
+          "pl" = ["obslog" "-p"];
+
+          # "cl" = ["git" "push" "-c" "@-"];
+          # "push" = ["git" "push" "--all"];
+
+          "configure" = ["config" "edit" "--repo"];
+
+          ".." = ["edit" "-r" "@-"];
+          ",," = ["edit" "-r" "@+"];
+        };
+        template-aliases = {
+          "format_timestamp(ts)" = ''
+            if(
+              ts.after("2 weeks ago"),
+              ts.ago(),
+              ts.format("%b %d, %Y %H:%M"),
+            )
+          '';
+        };
+        templates = {
+          log_node = ''
+            coalesce(
+              if(current_working_copy, "●"),
+              if(immutable, "⊗", "○"),
+            )
+          '';
+        };
         # revsets = {
         #   log = "@ | bases | branches | curbranch::@ | @::nextbranch | downstream(@, branchesandheads)";
-        # };
-        # revset-aliases = {
-        #   "bases" = "master";
-        #   "downstream(x,y)" = "(x::y) & y";
-        #   "branches" = "downstream(trunk(), bookmarks()) & mine()";
-        #   "branchesandheads" = "branches | (heads(trunk()::) & mine())";
-        #   "currbranch" = "latest(branches::@- & branches)";
-        #   "nextbranch" = "roots(@:: & branchesandheads)";
         # };
       };
     };
