6a9ef97e1b7b64a8d1eaea8261aaf2e04cfb4c9e
reo101 <pavel.atanasov2001@gmail.com>
2025-04-06 18:29:00 +0300

    feat(taskopen): use `infuse` to override `meta.platforms`

--- hosts/nix-darwin/limonka/homeModules/taskwarrior.nix
+++ hosts/nix-darwin/limonka/homeModules/taskwarrior.nix
@@ -1,13 +1,11 @@
 { inputs, lib, pkgs, config, ... }:
 
 {
-  home.packages = with pkgs; [
-    (taskopen.overrideAttrs (oldAttrs: {
-      meta = oldAttrs.meta // {
-        # NOTE: marked as `linux` only, but also works on `darwin`
-        platforms = lib.platforms.unix;
-      };
-    }))
+  home.packages = [
+    (lib.infuse pkgs.taskopen {
+      # NOTE: marked as `linux` only, but also works on `darwin`
+      __output.meta.platforms.__assign = lib.platforms.unix;
+    })
   ];
 
   programs.taskwarrior = {
