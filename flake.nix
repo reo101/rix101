6a5924e61364258a1a8b6a32943ce1c91dd37527
reo101 <pavel.atanasov2001@gmail.com>
2026-02-05 16:31:41 +0200

    chore(flake): format with `nixfmt`

--- flake.nix
+++ flake.nix
@@ -1,62 +1,68 @@
 {
   description = "reo101's NixOS, nix-on-droid and nix-darwin configs";
 
-  outputs = inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } ({ withSystem, flake-parts-lib, ... }: {
-      systems = import inputs.systems.outPath;
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+      { withSystem, flake-parts-lib, ... }:
+      {
+        systems = import inputs.systems.outPath;
 
-      # BUG: infinite recursion
-      # imports = [
-      #   ./modules/flake/modules
-      # ] ++ inputs.self.flakeModules;
+        # BUG: infinite recursion
+        # imports = [
+        #   ./modules/flake/modules
+        # ] ++ inputs.self.flakeModules;
 
-      imports = [
-        ./modules/flake/lib
-        ./modules/flake/lib-custom
-        ./modules/flake/pkgs
-        ./modules/flake/modules
-        ./modules/flake/configurations
-        ./modules/flake/agenix
-        ./modules/flake/topology
-        ./modules/flake/packages
-        ./modules/flake/overlays
-        ./modules/flake/shells
-      ];
+        imports = [
+          ./modules/flake/lib
+          ./modules/flake/lib-custom
+          ./modules/flake/pkgs
+          ./modules/flake/modules
+          ./modules/flake/configurations
+          ./modules/flake/agenix
+          ./modules/flake/topology
+          ./modules/flake/packages
+          ./modules/flake/overlays
+          ./modules/flake/shells
+        ];
 
-      auto = {
-        # Automatic modules, see `./modules/flake/modules/default.nix`
-        modules.enableAll = true;
+        auto = {
+          # Automatic modules, see `./modules/flake/modules/default.nix`
+          modules.enableAll = true;
 
-        # Automatic configurations, see `./modules/flake/configurations/default.nix`
-        configurations.enableAll = true;
+          # Automatic configurations, see `./modules/flake/configurations/default.nix`
+          configurations.enableAll = true;
 
-        # Automatic packages, see `./modules/flake/packages/default.nix`
-        packages.enable = true;
+          # Automatic packages, see `./modules/flake/packages/default.nix`
+          packages.enable = true;
 
-        # Automatic overlays, see `./modules/flake/overlays/default.nix`
-        overlays.enable = true;
+          # Automatic overlays, see `./modules/flake/overlays/default.nix`
+          overlays.enable = true;
 
-        # Automatic devShells, see `./modules/flake/shells/default.nix`
-        devShells.enable = true;
-      };
-
-      perSystem = { lib, pkgs, system, ... }: {
-        # Apps (`nix run`)
-        apps = import ./apps { inherit pkgs; };
-
-        # Formatter (`nix fmt`)
-        formatter = pkgs.nixfmt;
-      };
-
-      flake = {
-        inherit (inputs) self;
-
-        # Templates
-        templates = import ./templates {
-          inherit inputs;
+          # Automatic devShells, see `./modules/flake/shells/default.nix`
+          devShells.enable = true;
         };
-      };
-    });
+
+        perSystem =
+          { pkgs, ... }:
+          {
+            # Apps (`nix run`)
+            apps = import ./apps { inherit pkgs; };
+
+            # Formatter (`nix fmt`)
+            formatter = pkgs.nixfmt;
+          };
+
+        flake = {
+          inherit (inputs) self;
+
+          # Templates
+          templates = import ./templates {
+            inherit inputs;
+          };
+        };
+      }
+    );
 
   inputs = {
     systems = {
