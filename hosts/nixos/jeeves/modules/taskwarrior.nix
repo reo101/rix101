8e7c131f375dd2fcb5170caf412a621448a5e9e7
reo101 <pavel.atanasov2001@gmail.com>
2026-02-05 16:31:41 +0200

    feat(hosts/nixos/jeeves): upgrade `taskwarrior` to `https`

--- hosts/nixos/jeeves/modules/taskwarrior.nix
+++ hosts/nixos/jeeves/modules/taskwarrior.nix
@@ -5,7 +5,9 @@
     port = 10222;
   };
 
-  services.nginx.virtualHosts."taskwarrior.jeeves.lan" = {
+  services.nginx.virtualHosts."taskwarrior.jeeves.reo101.xyz" = {
+    forceSSL = true;
+    useACMEHost = "jeeves.reo101.xyz";
     locations."/" = {
       proxyPass = "http://127.0.0.1:${builtins.toString config.services.taskchampion-sync-server.port}";
       proxyWebsockets = true;
