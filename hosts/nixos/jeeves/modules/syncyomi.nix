4a1671bfb6deadf8f8f807dcfa4ed1f15f6c6001
reo101 <pavel.atanasov2001@gmail.com>
2026-02-05 16:31:41 +0200

    feat(hosts/jeeves): upgrade `syncyomi` to `https`

--- hosts/nixos/jeeves/modules/syncyomi.nix
+++ hosts/nixos/jeeves/modules/syncyomi.nix
@@ -5,9 +5,9 @@
   };
 
   services.nginx = {
-    virtualHosts."syncyomi.jeeves.lan" = {
-      enableACME = false;
-      forceSSL = false;
+    virtualHosts."syncyomi.jeeves.reo101.xyz" = {
+      forceSSL = true;
+      useACMEHost = "jeeves.reo101.xyz";
       locations."/" = {
         proxyPass = "http://127.0.0.1:${builtins.toString config.services.syncyomi.port}";
         proxyWebsockets = true;
