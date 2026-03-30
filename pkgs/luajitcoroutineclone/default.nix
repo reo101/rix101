597b5035fa6181869f61296752b7434c884237d7
reo101 <pavel.atanasov2001@gmail.com>
2026-03-30 18:22:49 +0300

    fix(pkgs/luajitcoroutineclone): harden

--- pkgs/luajitcoroutineclone/default.nix
+++ pkgs/luajitcoroutineclone/default.nix
@@ -1,7 +1,13 @@
-{ lib, luajitPackages }:
+{ luajit, lib }:
 
-luajitPackages.lua.overrideAttrs (oldAttrs: {
-  patches = (oldAttrs.patches or [ ]) ++ [
-    ./coroutine_clone.patch
-  ];
-})
+lib.fix (
+  patchedLuajit:
+  (luajit.overrideAttrs (oldAttrs: {
+    patches = (oldAttrs.patches or [ ]) ++ [
+      ./coroutine_clone.patch
+    ];
+  })).override
+    {
+      self = patchedLuajit;
+    }
+)
