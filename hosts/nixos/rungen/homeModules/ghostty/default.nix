61e79ee0f52f2104d46707f4af7bbf5696b80e4e
reo101 <pavel.atanasov2001@gmail.com>
2026-04-20 02:32:12 +0300

    feat(hosts/nixos/rungen): add Ghostty `font-codepoint-map` for `Uiua386`

--- hosts/nixos/rungen/homeModules/ghostty/default.nix
+++ hosts/nixos/rungen/homeModules/ghostty/default.nix
@@ -6,25 +6,38 @@
   ...
 }:
 
+let
+  uiuaCodepointMap =
+    (lib.concatStringsSep "," (import ./uiua386glyphs.nix))
+    + "=Uiua386";
+in
 {
   programs.ghostty = {
     enable = true;
     package = pkgs.ghostty;
     settings = {
       background-opacity = 0.65;
-      font-family = "Maple Mono NF CN";
+      font-family = [
+        "Maple Mono NF CN"
+      ];
+      font-codepoint-map = [
+        uiuaCodepointMap
+      ];
       adjust-box-thickness = 2;
       gtk-single-instance = true;
       keybind = [
         "shift+enter=text:\\x1b\\r"
       ];
-      custom-shader = lib.pipe [
-        "cursor_jumpy.glsl"
-        "sharpen.glsl"
-      ] [
-         (lib.map (lib.path.append ./shaders))
-         (lib.map builtins.toString)
-      ];
+      custom-shader =
+        lib.pipe
+          [
+            "cursor_jumpy.glsl"
+            "sharpen.glsl"
+          ]
+          [
+            (lib.map (lib.path.append ./shaders))
+            (lib.map builtins.toString)
+          ];
     };
     systemd = {
       enable = true;
