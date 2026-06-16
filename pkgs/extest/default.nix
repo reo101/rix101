15ad1e52010741e20321a28ed92125682cf06d5b
reo101 <pavel.atanasov2001@gmail.com>
2026-06-16 22:34:56 +0300

    refactor(pkgs/extest): make native, add compositor fallback patch
    - Steam is now 64-bit, so we don't ened to `pkgsi686Linux` anymore
    - Add patch to stop Steam from crashing
      when `niri` doesn't provide Wayland outputs info

--- pkgs/extest/default.nix
+++ pkgs/extest/default.nix
@@ -1,31 +1,29 @@
-{ pkgsi686Linux }:
+{
+  lib,
+  fetchFromGitHub,
+  rustPlatform,
+}:
 
-pkgsi686Linux.callPackage (
-  {
-    lib,
-    fetchFromGitHub,
-    rustPlatform,
-  }:
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "extest";
+  version = "1.0.3";
 
-  rustPlatform.buildRustPackage (finalAttrs: {
-    pname = "extest";
-    version = "1.0.3";
+  src = fetchFromGitHub {
+    owner = "Supreeeme";
+    repo = "extest";
+    rev = finalAttrs.version;
+    hash = "sha256-4SVZD0aHKsn97B5bhCf7URR6iQhJlYGALKWhDg+lGhU=";
+  };
 
-    src = fetchFromGitHub {
-      owner = "Supreeeme";
-      repo = "extest";
-      rev = finalAttrs.version;
-      hash = "sha256-4SVZD0aHKsn97B5bhCf7URR6iQhJlYGALKWhDg+lGhU=";
-    };
+  cargoHash = "sha256-OBWgNQ3OfqztaQwbK4fjOp7Lbu58U6j8tbStJ17bIko=";
 
-    cargoHash = "sha256-OBWgNQ3OfqztaQwbK4fjOp7Lbu58U6j8tbStJ17bIko=";
+  patches = [ ./no-compositor-fallback.patch ];
 
-    meta = {
-      description = "X11 XTEST reimplementation primarily for Steam Controller on Wayland";
-      homepage = "https://github.com/Supreeeme/extest";
-      platforms = lib.platforms.linux;
-      license = lib.licenses.mit;
-      maintainers = with lib.maintainers; [ reo101 ];
-    };
-  })
-) { }
+  meta = {
+    description = "X11 XTEST reimplementation primarily for Steam Controller on Wayland";
+    homepage = "https://github.com/Supreeeme/extest";
+    platforms = lib.platforms.linux;
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ reo101 ];
+  };
+})
