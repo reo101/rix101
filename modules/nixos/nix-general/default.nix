ec1245d62e9c88b70fddb348413d2c5ff0055f2b
reo101 <pavel.atanasov2001@gmail.com>
2026-07-09 22:55:05 +0300

    feat(modules/nixos/nix-general): add default `substituters`

--- modules/nixos/nix-general/default.nix
+++ modules/nixos/nix-general/default.nix
@@ -14,6 +14,15 @@
         "flakes"
       ];
 
+      substituters = lib.mkBefore [
+        "https://nix-community.cachix.org"
+        "https://rix101.cachix.org"
+      ];
+      trusted-public-keys = lib.mkBefore [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "rix101.cachix.org-1:2u9ZGi93zY3hJXQyoHkNBZpJK+GiXQyYf9J5TLzCpFY="
+      ];
+
       auto-optimise-store = lib.mkDefault true;
       keep-outputs = lib.mkDefault true;
       keep-derivations = lib.mkDefault true;
