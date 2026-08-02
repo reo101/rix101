a5668735af65169d929d617ef0f1a28e0fe3dde9
reo101 <pavel.atanasov2001@gmail.com>
2026-08-02 13:30:45 +0300

    fix(hosts/nixos/rungen): grant video group backlight access

--- hosts/nixos/rungen/modules/usb.nix
+++ hosts/nixos/rungen/modules/usb.nix
@@ -5,4 +5,9 @@
     pkgs.android-tools
     pkgs.logitech-udev-rules
   ];
+
+  services.udev.extraRules = ''
+    # Allow video group to control backlight (on add and change events)
+    ACTION=="add|change", SUBSYSTEM=="backlight", ENV{DEVNAME}=="/dev/null", ATTR{brightness}="*", GROUP="video", MODE="0664"
+  '';
 }
